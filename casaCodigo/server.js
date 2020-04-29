e809239c620f3a5d2a2b8ccafff280ad718d9ea0
Rayanne Oliveira <rayanneo390@gmail.com>
2020-04-29 11:49:39 -0300

    Projeto Casa do codigo

---
+++ casaCodigo/server.js
@@ -0,0 +1,37 @@
+ const express = require('express');
+
+const app = express();
+
+app.listen(3000,function(){
+ console.log('Servidor rodando na porta 3000');
+});
+
+app.get('/',function(req,resp){
+    resp.send('<html> <head> <meta charset="utf-8"> </head> <body> <h1>Casa do código</h1></body></html>');
+});
+
+app.get('/livros',function(req,resp){
+    resp.send('<html> <head> <meta charset="utf-8"> </head> <body> <h1>Casa do livro</h1></body></html>');
+});
+
+
+/*
+ const http = require('http');
+
+const servidor =  http.createServer(function (req,resp){
+
+    let html='';
+    if(req.url=='/'){
+
+        html= '<html> <head> <meta charset="utf-8"> </head> <body> <h1>Casa do código</h1></body></html>';
+    }
+    else if(req.url=='/livros'){
+        html= '<html> <head> <meta charset="utf-8"> </head> <body> <h1>Casa do livro</h1></body></html>';
+
+    }
+    resp.end(html);
+
+});
+
+servidor.listen(3000);
+*/
